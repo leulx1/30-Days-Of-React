97fab1f6c573dd635f4b7a527c36b4e1dc87e669
leulx1 <leultsehaye123@gmai.com>
2024-07-25 15:53:59 +0300

    finished some question of the level one

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,14 @@
+let array= [];
+let array2= [1,3,4,5,6];
+console.log(array2.length);
+console.log(array2[0],array2[2],array2[4])
+let mixedDataTypes=['idk','1','3','idk2',null,NaN,undefined];  console.log(mixedDataTypes.length)
+let itCompanies =['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+console.log(itCompanies)
+console.log(itCompanies.length)
+console.log(itCompanies[0],itCompanies[3],itCompanies[6])
+for (let i=0;i<itCompanies.length;i++){ console.log(itCompanies[i])}
+for(let i=0; i<itCompanies.length; i++){ console.log(itCompanies[i].toUpperCase())}
+console.log(itCompanies[0],itCompanies[1],itCompanies[2],itCompanies[3],itCompanies[4],itCompanies[5]+'and'+itCompanies[6] +'are big IT companies')
+
+
